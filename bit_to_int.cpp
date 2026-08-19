507dc8e257cbab99fc88053145c9b34512da29f5
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-19 12:48:08 +0530

    New prob bit_to_int.

---
+++ bit_to_int.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+int main() {
+    int n;
+    cout << "Input the binary val: ";
+    cin >> n;
+    int m = n;
+    int i = 0;
+    int result = 0;
+    while(m!=0) {
+
+    }
+    return 0;
+}
