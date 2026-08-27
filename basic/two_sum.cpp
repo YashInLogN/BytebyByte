e4913dbb9d39688dcc0609210a1acbf7b78cf4ce
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-28 04:13:08 +0530

    two_sum.cpp saved

--- basic/two_sum.cpp
+++ basic/two_sum.cpp
@@ -9,12 +9,16 @@
 #include <unordered_map>
 using namespace std;
 
-vector<int> twoSum(vector<int> &nums, int target){
-    unordered_map<int, int> vals;
-    for(int i = 0; i < nums.size(); i++) {
-        int compliment = target - nums[i];
-        
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> seen; // value -> index
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (seen.find(complement) != seen.end()) {
+            return {seen[complement], i};
+        }
+        seen[nums[i]] = i;
     }
+    return {}; // no solution found (won't happen per problem constraints)
 }
 
 int main() {
