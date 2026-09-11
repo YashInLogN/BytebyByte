b11497a5918f96f9db0d7e2803263382e82641f7
YashBuilds <chaudharyyashkakran@gmail.com>
2026-09-12 05:04:36 +0530

    Remove all occurence of a substring.

---
+++ Strings/remove_substring.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string removeSubstring(string str, string sub) {
+    size_t pos = str.find(sub);
+    while (pos != string::npos) {
+        str.erase(pos, sub.length());
+        pos = str.find(sub);
+    }
+    return str;
+}
+
+string removeSubstring2(string str, string sub) {
+    vector<char> stack;
+    int n = sub.size();
+    for(auto c: str){
+        stack.push_back(c);
+        if(stack.size() >= n){
+            bool match = true;
+            for(int i = 0; i < n; i++){
+                if(stack[stack.size() - n + i] != sub[i]){
+                    match = false;
+                    break;
+                }
+            }
+            if(match){
+                stack.resize(stack.size() - n);
+            }
+        }
+    }
+    string result;
+    for(auto c: stack){
+        result += c;
+    }
+    return result;
+}
+
+int main() {
+    string input, substring;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    cout << "Enter the substring to remove: ";
+    getline(cin, substring);
+    string result = removeSubstring2(input, substring);
+    cout << "String after removing substring: " << result << endl;
+    return 0;
+}
