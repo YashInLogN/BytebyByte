76ddce4302bc3049df0bc56c221b870bb637b47c
YashBuilds <chaudharyyashkakran@gmail.com>
2026-09-15 21:51:15 +0530

    Testing functions.

--- test.cpp
+++ test.cpp
@@ -86,6 +86,26 @@ string reverseWords(string &str){
 
 }
 
+string removeSubstring(const string& str, const string& sub){
+    string stack;
+    int n = sub.size();
+    for(auto c: str){
+        stack.push_back(c);
+        if(stack.size() >= n){
+            bool match = true;
+            for(int i = 0; i < n; i++){
+                if(stack[stack.size() - n + i] != sub[i]){
+                    match = false;
+                    break;
+                }
+            }if(match){
+                stack.resize(stack.size() - n);
+            }
+        }
+    }
+    return stack;
+}
+
 int main() {
     string input;
     cout << "Enter a string: ";
